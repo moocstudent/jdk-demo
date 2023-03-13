642ac55aaa77d218139ef1a579629ff000d5a566
moocstudent <deadzq@qq.com>
2023-03-13 16:12:26 +0800

    Initial commit

---
+++ src/main/java/com/example/jdkdemo/JdkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdkdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdkDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdkDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jdkdemo/JdkDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdkdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdkDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
